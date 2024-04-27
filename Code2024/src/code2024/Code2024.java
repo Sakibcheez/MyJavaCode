eab61719b477867fac623288d74b708628eef786
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-04-28 00:47:27 +0600

    new Project begins

---
+++ Code2024/src/code2024/Code2024.java
@@ -0,0 +1,13 @@
+
+package code2024;
+
+
+public class Code2024 {
+
+    
+    public static void main(String[] args) {
+       
+        System.out.println("This is my another new project !!");
+    }
+    
+}
