46121b06309b5be63b1f86d80ca5ce308aac733d
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-04-28 00:59:07 +0600

    summation

---
+++ Code2024/src/code2024/Summation1.java
@@ -0,0 +1,25 @@
+
+package code2024;
+
+import java.util.Scanner;
+
+
+public class Summation1 {
+    
+    public static void main(String[] args) {
+        
+     
+        System.out.println("Enter two digit: ");
+        
+      
+        Scanner scan = new Scanner(System.in);
+        
+        int x = scan.nextInt();
+                int y = scan.nextInt();
+                        int sum = x + y ;
+                        System.out.println("Summation is :" +sum);
+                        
+        
+    }
+    
+}
