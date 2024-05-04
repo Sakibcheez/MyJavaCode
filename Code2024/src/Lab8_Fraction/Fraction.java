5aca4ad37b04afcf4123ca2c78cb7d24b3e0c51a
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-05-04 23:49:45 +0600

    facing sub and div problem

--- Code2024/src/Lab8_Fraction/Fraction.java
+++ Code2024/src/Lab8_Fraction/Fraction.java
@@ -3,114 +3,95 @@ package Lab8_Fraction;
 
 public class Fraction {
     
-    private int numerator;
-    private int denominator;
 
-    // Constructor
-    public Fraction(int numerator, int denominator) {
-        this.numerator = numerator;
-        if (denominator != 0)
-            this.denominator = denominator;
-        else
-            throw new IllegalArgumentException("Denominator cannot be zero.");
+  private int numerator;
+  private int denominator;
+
+  // Constructor
+  public Fraction(int numerator, int denominator) {
+    if (denominator == 0) {
+      throw new IllegalArgumentException("Denominator cannot be zero.");
+    }
+    this.numerator = numerator;
+    this.denominator = denominator;
+    simplify(); // Simplify the fraction upon creation
+  }
+
+  // Getters
+  public int getNumerator() {
+    return numerator;
+  }
+
+  public int getDenominator() {
+    return denominator;
+  }
+
+
+  // Arithmetic operations with simplification
+  public void add(Fraction other) {
+    int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+    int newDenominator = this.denominator * other.denominator;
+    setFraction(newNumerator, newDenominator);
+  }
+
+  public void subtract(Fraction other) {
+    int newNumerator = this.numerator * other.denominator - other.numerator * this.denominator;
+    int newDenominator = this.denominator * other.denominator;
+    setFraction(newNumerator, newDenominator);
+  }
+
+  public void multiply(Fraction other) {
+    int newNumerator = this.numerator * other.numerator;
+    int newDenominator = this.denominator * other.denominator;
+    setFraction(newNumerator, newDenominator);
+  }
+
+  public void divide(Fraction other) {
+    if (other.numerator == 0) {
+      throw new IllegalArgumentException("Cannot divide by zero.");
+    }
+    int newNumerator = this.numerator * other.denominator;
+    int newDenominator = this.denominator * other.numerator;
+    setFraction(newNumerator, newDenominator);
+  }
+
+  // Helper method to set fraction and simplify
+  private void setFraction(int newNumerator, int newDenominator) {
+    this.numerator = newNumerator;
+    this.denominator = newDenominator;
+    simplify();
+  }
+
+  // Greatest Common Divisor (GCD)
+  private static int gcd(int a, int b) {
+    while (b != 0) {
+      int temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  }
+
+  // Simplify the fraction
+  private void simplify() {
+    int gcd = gcd(Math.abs(numerator), Math.abs(denominator));
+    if (gcd != 1) {
+      numerator /= gcd;
+      denominator /= gcd;
     }
 
-    // Getters and setters
-
-    public int getNumerator() {
-        return numerator;
+    // Handle negative signs
+    if (denominator < 0) {
+      numerator = -numerator;
+      denominator = -denominator;
     }
+  }
 
-    public int getDenominator() {
-        return denominator;
-    }
-
-    public void setNumerator(int numerator) {
-        this.numerator = numerator;
-    }
-
-    public void setDenominator(int denominator) {
-        if (denominator != 0)
-            this.denominator = denominator;
-        else
-            throw new IllegalArgumentException("Denominator cannot be zero.");
-    }
-
-    // Arithmetic operations
-
-    public void add(Fraction fraction) {
-        int newNumerator = this.numerator * fraction.getDenominator() +
-                fraction.getNumerator() * this.denominator;
-        int newDenominator = this.denominator * fraction.getDenominator();
-        simplifyAndSet(newNumerator, newDenominator);
-    }
-
-    public void sub(Fraction fraction) {
-        int newNumerator = this.numerator * fraction.getDenominator() -
-                (fraction.getNumerator() * this.denominator); 
-        int newDenominator = this.denominator * fraction.getDenominator();
-        simplifyAndSet(newNumerator, newDenominator);
-    }
-
-    public void multiplication(Fraction fraction) {
-        int newNumerator = this.numerator * fraction.getNumerator();
-        int newDenominator = this.denominator * fraction.getDenominator();
-        simplifyAndSet(newNumerator, newDenominator);
-    }
-
-    public void division(Fraction fraction) {
-        if (fraction.getNumerator() != 0) {
-            int newNumerator = this.numerator * fraction.getDenominator();
-            int newDenominator = this.denominator * fraction.getNumerator();
-            simplifyAndSet(newNumerator, newDenominator);
-        } else {
-            throw new IllegalArgumentException("Cannot divide by zero.");
-        }
-    }
-/*
-    private void simplifyAndSet(int newNumerator, int newDenominator) {
-        int gcd = gcd(Math.abs(newNumerator), Math.abs(newDenominator));
-        this.numerator = newNumerator / gcd;
-        this.denominator = newDenominator / gcd;
-    }
-*/  
-    
-   private void simplifyAndSet(int newNumerator, int newDenominator) {
-    if (newDenominator < 0) {
-        newNumerator = -newNumerator;
-        newDenominator = -newDenominator;
-    }
-    int gcd = gcd(Math.abs(newNumerator), Math.abs(newDenominator));
-    this.numerator = newNumerator / gcd;
-    this.denominator = newDenominator / gcd;
-}
-
-
-  private int gcd(int a, int b) {
-    int gcd = 1; // Initialize gcd to 1
-
-    for (int i = 1; i <= a && i <= b; i++) {
-        if (a % i == 0 && b % i == 0) {
-            gcd = i; // Update gcd if i is a common divisor
-        }
-    }
-
-    return gcd;
-}
-    
-    /* 
-     while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-    */
-
-    @Override
-    public String toString() {
-        return numerator + "/" + denominator;
-    }
-    
+  // Override toString for better representation
+  @Override
+  public String toString() {
+    return numerator + "/" + denominator;
+  }
     // Display information about fractions
     public static void displayInfo(Fraction f1, Fraction f2) {
         // Performing arithmetic operations
@@ -124,18 +105,18 @@ public class Fraction {
         System.out.println("Fraction 1: " + f1.toString());
 
         // Subtracting fractions
-        f1.sub(f2);
+        f1.subtract(f2);
         System.out.println("\nAfter subtracting fraction 2 from fraction 1:");
         System.out.println("Fraction 1: " + f1.toString());
 
         // Multiplying fractions
-        f1.multiplication(f2);
+        f1.multiply(f2);
         System.out.println("\nAfter multiplying fraction 1 by fraction 2:");
         System.out.println("Fraction 1: " + f1.toString());
 
         // Dividing fractions
         try {
-            f1.division(f2);
+            f1.divide(f2);
             System.out.println("\nAfter dividing fraction 1 by fraction 2:");
             System.out.println("Fraction 1: " + f1.toString());
         } catch (IllegalArgumentException e) {
