e5da4ec30bc638b756e64abe70830a9a2eb7ab62
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-05-06 22:31:01 +0600

    .

--- Code2024/src/code2024/oneinone.java
+++ Code2024/src/code2024/oneinone.java
@@ -1,36 +1,47 @@
 
 package code2024;
 
-
+//Object Casting 
 public class oneinone {
     
     public static void main(String[] args) {
-        m(new GraduateStudent());
+        m(new GraduateStudent()); //Class passing through a method 
         m(new Student());
         m(new Person());
         m(new Object());
         
     }
     
-    public static void m(Object x)
+    /**
+     *
+     * @param x
+     */
+    
+
+//In Java documentation comments, @param is a tag used to describe a method or constructor parameter.
+//It provides information about the purpose and expected behavior of a particular parameter.
+
+    public static void m(Object x) //Object Class type Variable 
     {
         System.out.println(x.toString());
     }
     
 }
 
-class GraduateStudent extends Student{
+class GraduateStudent extends Student
+{
 
 }
 
-    class Student extends Person{
-    @Override
+class Student extends Person
+{
+@Override
 public String toString(){
     return "Student";
 }
 }
 
-class Person extends Object 
+class Person extends Object // java object Class 
 {
 @Override
 
@@ -38,4 +49,5 @@ public String toString(){
 
 return "Person";
 }
+
 }
