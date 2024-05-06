0a3d3992dc1d5565ca406bb27ed2ba72189bd93d
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-05-07 02:19:57 +0600

    edit

--- Code2024/src/ArrayPractice/Array_Reverse.java
+++ Code2024/src/ArrayPractice/Array_Reverse.java
@@ -1,12 +1,19 @@
 
 package ArrayPractice;
 
+import java.util.Arrays;
+
 
 public class Array_Reverse {
     public static void main(String[] args) {
        
         int[] numberList = {1, 2, 3, 4, 5, 6};
-        reverseArr(numberList);
+        
+        System.out.println(Arrays.toString(numberList));
+        
+        System.out.println("\nReverse \n");
+        reverseArr(numberList); 
+        
         for (int i = 0; i < numberList.length; i++) {
             System.out.print(numberList[i]);
             if (i < numberList.length - 1) {
