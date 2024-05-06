197f3164d9cd853185af1147150c1cf9bed995d5
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-05-06 20:21:37 +0600

    matrix1

---
+++ Code2024/src/ArrayPractice/Matrix1.java
@@ -0,0 +1,45 @@
+
+package ArrayPractice;
+
+import java.util.Scanner;
+
+
+public class Matrix1 {
+    
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Create a 3x3 matrix
+        int[][] matrix = new int[3][3];
+
+        // Take input for the matrix
+        System.out.println("Enter the elements of the 3x3 matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        // Display the matrix
+        System.out.println("Matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Calculate the sum of the main diagonal elements
+        int sumMainDiagonal = 0;
+        for (int i = 0; i < 3; i++) {
+            sumMainDiagonal += matrix[i][i];
+        }
+
+        // Display the sum of the main diagonal elements
+        System.out.println("Sum of the main diagonal elements: " + sumMainDiagonal);
+
+        scanner.close();
+    }
+    
+    
+}
