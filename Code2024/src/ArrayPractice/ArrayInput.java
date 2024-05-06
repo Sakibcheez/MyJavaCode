cc20d9b1de1713ef8b4404f4b4c69222db3f08c6
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-05-06 16:00:38 +0600

    small edit

--- Code2024/src/ArrayPractice/ArrayInput.java
+++ Code2024/src/ArrayPractice/ArrayInput.java
@@ -17,23 +17,25 @@ public class ArrayInput {
             System.out.println("Enter the elements of the 1D array:");
             for (int i = 0; i < length1D; i++) {
                 array1D[i] = scanner.nextInt();
-                
-                
-            }   // 2D Array
+                     
+            }  
+            // 2D Array
             System.out.println("\n2D Array Example:");
             System.out.println("Enter the number of rows for the 2D array:");
             int rows2D = scanner.nextInt();
             System.out.println("Enter the number of columns for the 2D array:");
             int columns2D = scanner.nextInt();
+            
             int[][] array2D = new int[rows2D][columns2D];
             System.out.println("Enter the elements of the 2D array:");
+            // fill up 1st row then 2nd then ...... 
             for (int i = 0; i < rows2D; i++) {
                 for (int j = 0; j < columns2D; j++) {
                     array2D[i][j] = scanner.nextInt();
                 }
                 
-                
-            }   // Ragged Array
+                }   
+        // Ragged Array
             System.out.println("\nRagged Array Example:");
             System.out.println("Enter the number of rows for the ragged array:");
             int rowsRagged = scanner.nextInt();
@@ -47,7 +49,10 @@ public class ArrayInput {
                 for (int j = 0; j < columnsRagged; j++) {
                     raggedArray[i][j] = scanner.nextInt();
                 }
-            }   // Displaying 1D Array
+            }  
+            //printing returl 
+            
+// Displaying 1D Array
             System.out.println("\n1D Array:");
             for (int i = 0; i < length1D; i++) {
                 System.out.print(array1D[i] + " ");
--- Code2024/src/ArrayPractice/ArrayLength1.java
+++ Code2024/src/ArrayPractice/ArrayLength1.java
@@ -64,6 +64,7 @@ public class ArrayLength1 {
     
     for(int i =0; i<distance1.length; i++)
     {
+        System.out.println("Row Number : "+(i+1)); 
      for(int j = 0; j<distance1[i].length ; j++){
          System.out.print(distance1[i][j] + " ");
      }        
