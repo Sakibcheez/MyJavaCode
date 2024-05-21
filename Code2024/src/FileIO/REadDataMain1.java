685815ab661c3655fb76b60b21d38270a76df7d9
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-05-22 00:11:01 +0600

    read io

---
+++ Code2024/src/FileIO/REadDataMain1.java
@@ -0,0 +1,34 @@
+
+package FileIO;
+
+import java.io.File;
+import java.util.Scanner;
+
+
+public class REadDataMain1 {
+    
+    public static void main(String[] args) {
+       
+    
+      File file = new File ("name.txt");
+        try
+        {
+            Scanner input = new Scanner(file);
+            
+            while(input.hasNext())
+            {
+                String firstName = input.next();
+                String lastName = input.next();
+                int id = input.nextInt();
+                
+                System.out.println("Name : "+firstName+" "+ lastName + "\tID: "+id);
+            }
+            input.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+}
