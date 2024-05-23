706e9be00d984fcf21cf3998db6ebebaebe818e8
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-05-24 02:07:19 +0600

    font

--- SwuingGuiNeatBeans/src/swuingguineatbeans/TextFieldDemo.java
+++ SwuingGuiNeatBeans/src/swuingguineatbeans/TextFieldDemo.java
@@ -3,6 +3,7 @@ package swuingguineatbeans;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
@@ -11,6 +12,7 @@ public class TextFieldDemo extends JFrame {
     
     private Container c;
     private JTextField tf1, tf2;
+    private Font f; 
     
     TextFieldDemo(){
     initComponents();
@@ -22,13 +24,19 @@ public class TextFieldDemo extends JFrame {
         c.setLayout(null);
         c.setBackground(Color.pink);
         
+        
+        f = new Font("Arial",Font.ITALIC + Font.BOLD, 14);  
+        
         tf1 = new JTextField();
         tf1.setBounds(50,50,200,50);
         c.add(tf1);
+        tf1.setFont(f);
         
         tf2 = new JTextField("hello sakib");
         tf2.setBounds(50, 120, 200, 50);
         c.add(tf2);
+        
+        
         
         
     
