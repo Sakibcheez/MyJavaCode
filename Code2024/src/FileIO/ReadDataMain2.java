8f2a35e76162e7252141932b1076f572d1e86aa3
SAKIB AHMED SHISHIR <sakib.shishir@northsouth.edu>
2024-06-09 15:04:20 +0600

    lab work up

---
+++ Code2024/src/FileIO/ReadDataMain2.java
@@ -0,0 +1,32 @@
+
+package FileIO;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ReadDataMain2 {
+    
+    public static void main(String[] args) throws Exception {
+        
+    
+      File file = new File ("name.txt");
+            Scanner input = new Scanner(file);
+            
+            while(input.hasNext())
+            {
+                String name = input.nextLine();
+                int id = input.nextInt();
+                
+                System.out.println("Name : "+name + "\tID: "+id);
+                
+                if(input.hasNext())
+                    input.nextLine();
+            }
+            input.close();
+        }
+       
+        
+    }
+    
+} 
+
